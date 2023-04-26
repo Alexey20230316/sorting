a372b1226e456d077dc7b8b05ab35330f2178f4b
Aleksey <qwe810@yandex.ru>
2023-04-26 16:20:08 +0700

    proverka

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -1,8 +1,88 @@
 #include<iostream>
+#include<string>
+#include<fstream>
+#include<Windows.h>
+
+class address {
+
+private:
+	std::string city;
+	std::string street;
+	int house;
+	int flat;
+
+public:
+	// геттеры
+	std::string get_city()const {
+		return city;
+	}
+	std::string get_street()const {
+		return street;
+	}
+	int get_house()const {
+		return house;
+	}
+	int get_flat()const {
+		return flat;
+	}
+
+	//сеттеры
+	void set_city(std::string citi) {
+		this->city = citi;
+	}
+	void set_street(std::string street) {
+		this->street = street;
+	}
+	void set_house(int house) {
+		this->house = house;
+	}
+	void set_flat(int flat) {
+		this->flat = flat;
+	}
+
+	//конструктор по умолчанию
+	address() {
+		city = "No name ";
+		street = "No name ";
+		house = 0;
+		flat = 0;
+	}
+
+};
+
 
 int main() {
 
 	setlocale(LC_ALL, "Russian");
+
+	std::string path_in = "in.txt";  // имя файла присваиваем переменной типа                                                               стринг 
+	std::ifstream fin;              //   создаем обьект fin на чтение
+	fin.open(path_in);             //  вызов метода  open для открытия файла  in.txt.
+
+	std::string path_out = "out.txt";   // имя файла присваиваем переменной типа стринг
+	std::ofstream fout;                //  создаем обьект fout на запись
+	fout.open(path_out);              //  вызов метода  open для открытия файла out.txt.
+
+	if (!(fin.is_open() || fout.is_open()))      //  is_open  возвращает  true или false
+	{
+		std::cout << "Ошибка открытия файла!" << std::endl;
+	}
+	else
+	{
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
 
 
 }
